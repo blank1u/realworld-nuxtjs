ee790e8b8ef61d6103980380801fa696e0988480
ganping <ganp1020@126.com>
2020-07-12 21:08:29 +0800

    feat: 模板以及样式

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+export default {
+    router: {
+        router: {
+            linkActiveClass: 'active'
+        },
+        extendRoutes(routes, resolve) {
+            routes.splice(0)
+            routes.push({
+                name: 'Layout',
+                path: '/',
+                component: resolve(__dirname, 'pages/layout/index.vue'),
+                children: [
+                    {
+                        name: 'Home',
+                        path: '',  // 默认 nuxt-child 路由
+                        component: resolve(__dirname, 'pages/layout/home.vue'),
+                    },
+                    {
+                        name: 'Login',
+                        path: 'login',
+                        component: resolve(__dirname, 'pages/login/index.vue'),
+                    },
+                    {
+                        name: 'Register',
+                        path: 'register',
+                        component: resolve(__dirname, 'pages/login/index.vue'),
+                    },
+                    {
+                        name: 'Profile',
+                        path: 'profile/:username',
+                        component: resolve(__dirname, 'pages/profile/index.vue'),
+                    },
+                    {
+                        name: 'Settings',
+                        path: '/settings',
+                        component: resolve(__dirname, 'pages/settings/index.vue'),
+                    },
+                    {
+                        name: 'Article',
+                        path: '/article/:id',
+                        component: resolve(__dirname, 'pages/article/index.vue'),
+                    },
+                    {
+                        name: 'Editor',
+                        path: '/editor/:id',
+                        component: resolve(__dirname, 'pages/editor/index.vue'),
+                    }
+                ]
+            })
+        }
+    }
+}
---
+++ pages/layout/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <main>
+        <nav class="navbar navbar-light">
+            <div class="container">
+                <nuxt-link class="navbar-brand" to="/">conduit</nuxt-link>
+                <ul class="nav navbar-nav pull-xs-right">
+                    <li class="nav-item">
+                        <nuxt-link class="nav-link" to="/">Home</nuxt-link>
+                        <!-- Add "active" class when you're on that page" -->
+                        <a class="" href=""></a>
+                    </li>
+                    <li class="nav-item">
+                        <nuxt-link class="nav-link" to="/editor/11">
+                            <i class="ion-compose"></i>&nbsp;New Post
+                        </nuxt-link>
+                    </li>
+                    <li class="nav-item">
+                        <nuxt-link class="nav-link" to="/settings">
+                            <i class="ion-gear-a"></i>&nbsp;Settings
+                        </nuxt-link>
+                    </li>
+                    <li class="nav-item">
+                        <nuxt-link tag="a" class="nav-link" to="/login">Sign up</nuxt-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <nuxt-child></nuxt-child>
+        <footer>
+            <div class="container">
+                <a href="/" class="logo-font">conduit</a>
+                <span class="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+            </div>
+        </footer>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
